83da552b9936605c97acc21d9517a8b17ab6f765
fhrsak <hrsak.filip@gmail.com>
2022-07-05 21:22:36 +0200

    Profiles

--- src/main/java/hr/hrsak/sfgdi/SfgDiApplication.java
+++ src/main/java/hr/hrsak/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package hr.hrsak.sfgdi;
 
-import hr.hrsak.sfgdi.controllers.ConstructorInjectedController;
-import hr.hrsak.sfgdi.controllers.MyController;
-import hr.hrsak.sfgdi.controllers.PropertyInjectedController;
-import hr.hrsak.sfgdi.controllers.SetterInjectedController;
+import hr.hrsak.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -19,7 +16,6 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
-		System.out.println("--------Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean(PropertyInjectedController.class);
 		System.out.println(propertyInjectedController.getGreeting());
 
@@ -28,6 +24,11 @@ public class SfgDiApplication {
 
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean(ConstructorInjectedController.class);
 		System.out.println(constructorInjectedController.getGreeting());
+
+		I18nController i18nController = (I18nController) ctx.getBean(I18nController.class);
+		System.out.println(i18nController.sayGreeting());
+
+
 	}
 
 }
--- src/main/java/hr/hrsak/sfgdi/controllers/I18nController.java
+++ src/main/java/hr/hrsak/sfgdi/controllers/I18nController.java
@@ -1,18 +1,20 @@
 package hr.hrsak.sfgdi.controllers;
 
 import hr.hrsak.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MyController {
+public class I18nController {
 
     private final GreetingService greetingService;
 
-    public MyController(GreetingService greetingService) {
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    public String sayHello(){
+    public String sayGreeting(){
         return greetingService.sayGreeting();
     }
 }
